3dbe74279479817635689f6829e66ef8f5631368
Viktor <swifrorlilko@gmail.com>
2020-03-17 18:49:52 +0200

    adding idea model and technology type

---
+++ Blog/Models/TechnologyType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models
+{
+    public enum TechnologyType
+    {
+        nodejs,
+        javascript,
+        java,
+        csharp,
+        aspnet,
+        python,
+        django,
+        flask,
+        php,
+        laravel,
+        winforms,
+        webforms,
+        electronjs,
+        unity,
+    }
+}
