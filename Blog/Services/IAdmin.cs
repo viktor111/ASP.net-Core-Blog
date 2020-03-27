4a66e4e676982317dbf8f3e4dbdbad1e87c568dd
viktor111 <swifrorlilko@gmail.com>
2020-03-27 21:01:26 +0200

    add users sql data

---
+++ Blog/Services/IAdmin.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Services
+{
+    public interface IAdmin
+    {
+        List<ApplicationUser> GetUsers();
+        ApplicationUser DelteUser(int id);
+        ApplicationUser RestrictUser(int id);
+        ApplicationUser UserDetails(int id);
+    }
+}
---
+++ Blog/Services/SqlAdminData.cs
@@ -0,0 +1,40 @@
+﻿using Blog.Data;
+using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Services
+{
+    public class SqlAdminData : IAdmin
+    {
+
+        private BlogDbContext _context;
+
+        public SqlAdminData(BlogDbContext context)
+        {
+            _context = context;
+        }
+
+        public ApplicationUser DelteUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ApplicationUser> GetUsers()
+        {
+            return _context.Users.OrderBy(u => u.Email).ToList();
+        }
+
+        public ApplicationUser RestrictUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicationUser UserDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
