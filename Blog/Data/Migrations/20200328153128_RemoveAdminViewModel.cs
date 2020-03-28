3272b88e0f02491b14a540ce008db77d4d0cd8c4
viktor111 <swifrorlilko@gmail.com>
2020-03-28 19:52:23 +0200

    adding details view for users

---
+++ Blog/Data/Migrations/20200328153128_RemoveAdminViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blog.Data.Migrations
+{
+    public partial class RemoveAdminViewModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_AdminViewModel_AdminViewModelId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "AdminViewModel");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_AdminViewModelId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "AdminViewModelId",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AdminViewModelId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "AdminViewModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdminViewModel", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_AdminViewModelId",
+                table: "AspNetUsers",
+                column: "AdminViewModelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_AdminViewModel_AdminViewModelId",
+                table: "AspNetUsers",
+                column: "AdminViewModelId",
+                principalTable: "AdminViewModel",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
