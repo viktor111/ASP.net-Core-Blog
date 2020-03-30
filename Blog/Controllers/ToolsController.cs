b9819c35788afe78bef2ac99346200f5ca7feeb3
viktor111 <swifrorlilko@gmail.com>
2020-03-30 23:52:07 +0300

    add ip look up

--- Blog/Controllers/ToolsController.cs
+++ Blog/Controllers/ToolsController.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
 {
     public class ToolsController : Controller
     {
+
+
+        public ToolsController()
+        {
+
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult IpLookUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult IpLookUp(ToolsViewModel toolsViewModel)
+        {
+            ToolsViewModel model = new ToolsViewModel();
+            model.Website = toolsViewModel.Website;
+
+            return View(model);
         }
     }
 }
--- Blog/Data/BlogDbContext.cs
+++ Blog/Data/BlogDbContext.cs
@@ -34,8 +34,6 @@ namespace Blog.Data
 
         public DbSet<Article> Articles { get; set; }
         public DbSet<Comment> Comments { get; set; }
-        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
-        //public DbSet<Blog.ViewModels.AdminViewModel> AdminViewModel { get; set; }
-        //public DbSet<Blog.ViewModels.ArticeViewModel> ArticeViewModel { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }        
     }
 }
