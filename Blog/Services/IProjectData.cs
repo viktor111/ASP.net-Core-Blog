44b8b5014ec0ab1ed83e2af60216c0e4dbd289b5
viktor111 <swifrorlilko@gmail.com>
2020-04-03 08:18:34 +0300

    add serivce for project

---
+++ Blog/Services/IProjectData.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Services
+{
+    public interface IProjectData
+    {
+        IEnumerable<Project> GetArticles();
+        Project GetArticle(int id);
+        Project PostArticle(Project article);
+        Project EdditArticle(Project article);
+        Project DeleteArticle(int id);
+    }
+}
---
+++ Blog/Services/SqlProjectData.cs
@@ -0,0 +1,46 @@
+﻿using Blog.Data;
+using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Services
+{
+    public class SqlProjectData : IProjectData
+    {
+        private BlogDbContext _context;
+
+        public SqlProjectData(BlogDbContext context)
+        {
+            _context = context;
+        }
+
+        public Project DeleteArticle(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Project EdditArticle(Project article)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Project GetArticle(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Project> GetArticles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Project PostArticle(Project project)
+        {
+            _context.Projects.Add(project);
+            _context.SaveChanges();
+            return project;
+        }
+    }
+}
